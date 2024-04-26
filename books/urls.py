5715e297f7a7aef8754b5044f9962d73a226f9a6
Zuhayr Abdullazhanov <115369065+loyaldept@users.noreply.github.com>
2024-04-26 01:31:16 -0400

    Delete books directory

--- books/urls.py
+++
@@ -1,7 +0,0 @@
-# books/urls.py - #@completed by Zuhayr Abdullazhanov
-from django.urls import path
-from .views import home, book_list
-urlpatterns = [
-    path('', home, name='home'),  # Root URL for the home page
-    path('books/', book_list, name='book_list'),  # URL for viewing the book list
-]
